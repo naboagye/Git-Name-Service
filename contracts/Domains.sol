604057803b4219fe4a9668acf7d8f7a57c61c9a8
naboagye <naboagye13@gmail.com>
2022-04-05 22:14:34 +0100

    first commit

---
+++ contracts/Domains.sol
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+// We first import some OpenZeppelin Contracts.
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import {StringUtils} from "./libraries/StringUtils.sol";
+// We import another help function
+import {Base64} from "./libraries/Base64.sol";
+
+import "hardhat/console.sol";
+
+// We inherit the contract we imported. This means we'll have access
+// to the inherited contract's methods.
+contract Domains is ERC721URIStorage {
+  // Magic given to us by OpenZeppelin to help us keep track of tokenIds.
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  string public tld;
+  
+  // We'll be storing our NFT images on chain as SVGs
+  string svgPartOne = '<svg xmlns="http://www.w3.org/2000/svg" width="270" height="270" fill="none"><path fill="url(#a)" d="M0 0h270v270H0z"/><defs><filter id="b" color-interpolation-filters="sRGB" filterUnits="userSpaceOnUse" height="270" width="270"><feDropShadow dx="0" dy="1" stdDeviation="2" flood-opacity=".225" width="200%" height="200%"/></filter></defs><path d="M60.5 18C39.794 18 23 34.79 23 55.5c0 16.569 10.744 30.625 25.647 35.584 1.872.347 2.478-.815 2.478-1.803V82.3c-10.431 2.269-12.603-4.425-12.603-4.425-1.706-4.334-4.166-5.487-4.166-5.487-3.403-2.329.26-2.279.26-2.279 3.765.263 5.746 3.866 5.746 3.866 3.344 5.731 8.772 4.075 10.913 3.116.334-2.422 1.306-4.078 2.381-5.013-8.328-.953-17.084-4.169-17.084-18.534 0-4.097 1.465-7.44 3.862-10.066-.387-.947-1.671-4.762.366-9.925 0 0 3.15-1.006 10.316 3.844 2.99-.831 6.197-1.247 9.384-1.263 3.188.016 6.397.432 9.394 1.263 7.16-4.85 10.303-3.844 10.303-3.844 2.04 5.166.756 8.981.369 9.925 2.406 2.625 3.859 5.972 3.859 10.066 0 14.403-8.772 17.575-17.122 18.503 1.344 1.162 2.572 3.444 2.572 6.944v10.29c0 .997.6 2.169 2.503 1.8C87.268 86.116 98 72.062 98 55.5 98 34.79 81.21 18 60.5 18Z" fill="#fff"/><defs><linearGradient id="a" x1="0" y1="0" x2="270" y2="270" gradientUnits="userSpaceOnUse"><stop stop-color="#0031AE"/><stop offset="1" stop-color="#E40C9B" stop-opacity=".99"/></linearGradient></defs><text x="32.5" y="231" font-size="27" fill="#fff" filter="url(#b)" font-family="Plus Jakarta Sans,DejaVu Sans,Noto Color Emoji,Apple Color Emoji,sans-serif" font-weight="bold">';
+  string svgPartTwo = '</text></svg>';
+
+  mapping(string => address) public domains;
+  mapping(string => string) public records;
+
+  constructor(string memory _tld) payable ERC721("Git Name Service", "GNS") {
+    tld = _tld;
+    console.log("%s name service deployed", _tld);
+  }
+
+  function register(string calldata name) public payable {
+    require(domains[name] == address(0));
+
+    uint256 _price = price(name);
+    require(msg.value >= _price, "Not enough Matic paid");
+    
+    // Combine the name passed into the function  with the TLD
+    string memory _name = string(abi.encodePacked(name, ".", tld));
+    // Create the SVG (image) for the NFT with the name
+    string memory finalSvg = string(abi.encodePacked(svgPartOne, _name, svgPartTwo));
+    uint256 newRecordId = _tokenIds.current();
+    uint256 length = StringUtils.strlen(name);
+    string memory strLen = Strings.toString(length);
+
+    console.log("Registering %s.%s on the contract with tokenID %d", name, tld, newRecordId);
+
+    // Create the JSON metadata of our NFT. We do this by combining strings and encoding as base64
+    string memory json = Base64.encode(
+      abi.encodePacked(
+        '{"name": "',
+        _name,
+        '", "description": "A domain on the Git name service", "image": "data:image/svg+xml;base64,',
+        Base64.encode(bytes(finalSvg)),
+        '","length":"',
+        strLen,
+        '"}'
+      )
+    );
+
+    string memory finalTokenUri = string( abi.encodePacked("data:application/json;base64,", json));
+
+    console.log("\n--------------------------------------------------------");
+    console.log("Final tokenURI", finalTokenUri);
+    console.log("--------------------------------------------------------\n");
+
+    _safeMint(msg.sender, newRecordId);
+    _setTokenURI(newRecordId, finalTokenUri);
+    domains[name] = msg.sender;
+
+    _tokenIds.increment();
+  }
+
+  // This function will give us the price of a domain based on length
+  function price(string calldata name) public pure returns(uint) {
+    uint len = StringUtils.strlen(name);
+    require(len > 0);
+    if (len == 3) {
+      return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+    } else if (len == 4) {
+      return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+    } else {
+      return 1 * 10**17;
+    }
+  }
+
+  function getAddress(string calldata name) public view returns (address) {
+      // Check that the owner is the transaction sender
+      return domains[name];
+  }
+
+  function setRecord(string calldata name, string calldata record) public {
+      // Check that the owner is the transaction sender
+      require(domains[name] == msg.sender);
+      records[name] = record;
+  }
+
+  function getRecord(string calldata name) public view returns(string memory) {
+      return records[name];
+  }
+}
